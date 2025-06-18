88b9452448fa9f63040635cf159186524feabd57
Nokubonga Ngema <156459137+CodeCrafterNoku@users.noreply.github.com>
2025-06-18 15:07:42 +0200

    Update factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,21 +2,11 @@
 import sys
 
 def factorial(n):
-    """
-    Function Description:
-    Calculates the factorial of a given integer.
+    result = 1
+    while n > 1:
+        result *= n
+        n = n - 1
+    return result
 
-    Parameters:
-    - n (int): The integer for which the factorial is to be calculated.
-
-    Returns:
-    - int: The factorial of the input integer 'n'.
-    """
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-# Getting input from command line argument and calculating factorial
 f = factorial(int(sys.argv[1]))
 print(f)
